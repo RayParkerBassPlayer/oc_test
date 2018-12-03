dc06941553ebc3a3200b5f57abe495554c558377
Ray Parker <RayParkerBassPlayer@gmail.com>
2018-12-03 14:38:05 -0500

    Simplified structure to reflect filename change.

--- spec/integration/shingle_browsing_start_state_spec.rb
+++ spec/integration/shingle_browsing_start_state_spec.rb
@@ -8,37 +8,35 @@ describe "Shingle Browsing Start State", :vcr do
   let(:shingle_values) {shingle_select_values.collect(&:last).sort}
   let(:shingle_dropdown_selector) {"#shingle-names"}
 
-  describe "page start state" do
-    it "shows the default shingle name" do
-      visit root_path
-      
-      expect(find("#shingle-name").text).to eq shingles.first["proper_name"]
+  it "shows the default shingle name" do
+    visit root_path
+
+    expect(find("#shingle-name").text).to eq shingles.first["proper_name"]
+  end
+
+  it "shows the default shingle name in the dropdown selector" do
+    visit root_path
+
+    expect(find(shingle_dropdown_selector).value).to eq shingle_values.first
+  end
+
+  it "shows the default shingle large image" do
+    visit root_path
+
+    within("#main-image") do
+      shingle_color = ShingleConnection.shingle_colors(shingles.first).first["house_thumb_image_url"]
+
+      expect(find("img")["src"]).to eq shingle_color
     end
-    
-    it "shows the default shingle name in the dropdown selector" do
-      visit root_path
+  end
 
-      expect(find(shingle_dropdown_selector).value).to eq shingle_values.first
-    end
+  it "shows the color swatches with the default shingle selected" do
+    visit root_path
 
-    it "shows the default shingle large image" do
-      visit root_path
+    within("#swatches") do
+      shingle_color = ShingleConnection.shingle_colors(shingles.first).first["swatch_160x160_url"]
 
-      within("#main-image") do
-        shingle_color = ShingleConnection.shingle_colors(shingles.first).first["house_thumb_image_url"]
-
-        expect(find("img")["src"]).to eq shingle_color
-      end
-    end
-
-    it "shows the color swatches with the default shingle selected" do
-      visit root_path
-
-      within("#swatches") do
-        shingle_color = ShingleConnection.shingle_colors(shingles.first).first["swatch_160x160_url"]
-
-        expect(find("img.selected")["src"]).to eq shingle_color
-      end
+      expect(find("img.selected")["src"]).to eq shingle_color
     end
   end
 end

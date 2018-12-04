b5b9563bf2086ec95bf5389a6abb0de0b9374d91
Ray Parker <RayParkerBassPlayer@gmail.com>
2018-12-04 15:40:04 -0500

    Refactor ad prefunctory check on #shingle_colors.

--- spec/lib/shingle_connection_spec.rb
+++ spec/lib/shingle_connection_spec.rb
@@ -2,17 +2,17 @@ require "rails_helper"
 require "shingle_connection"
 
 describe ShingleConnection, :vcr do
-  it "retrieves a list of singles" do
-    response = ShingleConnection.shingles
+  let(:shingles){ShingleConnection.shingles}
 
-    expect(response).to be_a(Array)
+  it "retrieves a list of singles" do
+    expect(shingles).to be_a(Array)
   end
 
   it "retrieves a list of singles with a zip code" do
-    response = ShingleConnection.shingles(:zip => "43551")
-
-    expect(response).to be_a(Array)
+    expect(shingles).to be_a(Array)
   end
 
-  it "extracts an ordered list of colors for a given shingle"
+  it "extracts an ordered list of colors for a given shingle" do
+    expect(ShingleConnection.shingle_colors(shingles.first)).to be_an(Array)
+  end
 end

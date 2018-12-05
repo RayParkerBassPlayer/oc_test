767dbb09d79b3dd92a14d4ac80fbc9a8233694cf
Ray Parker <RayParkerBassPlayer@gmail.com>
2018-12-05 12:47:12 -0500

    Zip codes added.

--- db/schema.rb
+++ db/schema.rb
@@ -10,6 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 2018_12_05_172042) do
+
+  create_table "zips", id: false, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "code"
+  end
 
 end

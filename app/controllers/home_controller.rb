4d8fcbc0bab209e48bc89095eb6d4b6d8ba3d92f
Ray Parker <RayParkerBassPlayer@gmail.com>
2018-12-05 13:10:02 -0500

    Zip field added.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,10 +2,17 @@ require "shingle_connection"
 
 class HomeController < ApplicationController
   def shingles
-    zip = params["zip"] || "43659"
-    @shingles = ShingleConnection.shingles(:zip => zip)
+    @current_zip = nil
 
-    @current_shingle = params["shingle"].present? ? @shingles.find{|shingle| shingle["uid"] == params["shingle"]} : @shingles.first
-    @current_color = @current_shingle["shingle_colors"].find{|color| color["uid"] == params["color"]} || @current_shingle["shingle_colors"].first
+    begin
+      @current_zip = params["zip"].present? ? Zip.find(params["zip"]) : Zip.find("43659")
+    rescue => e
+      raise "Zip code not found. (#{e})"
+    end
+
+    @shingles = ShingleConnection.shingles(:zip => @current_zip.code)
+
+    @current_shingle = params["shingle"].present? ? @shingles.find { |shingle| shingle["uid"] == params["shingle"] } : @shingles.first
+    @current_color = @current_shingle["shingle_colors"].find { |color| color["uid"] == params["color"] } || @current_shingle["shingle_colors"].first
   end
 end

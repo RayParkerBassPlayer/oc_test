d6e47d48b08a983ed9b632ddf9a90ec1c632ecd1
Ray Parker <RayParkerBassPlayer@gmail.com>
2018-12-08 11:08:04 -0500

    Fixed spec.

--- spec/models/favorite_spec.rb
+++ spec/models/favorite_spec.rb
@@ -12,15 +12,16 @@ describe Favorite do
 
   describe "favoriting", :vcr do
     it "stows an url and the favoriteer's ip" do
-      favorite = create(:favorite)
+      favorite = build(:favorite)
+      dp favorite
       expect{Favorite.add(favorite.url, favorite.ip, :shingle => favorite.shingle, :color => favorite.color)}.to change{Favorite.count}.by(1)
     end
 
     it "silently allows a duplicate like to be added, but not stored" do
-      favorite = create(:favorite)
+      favorite = build(:favorite)
 
-      expect{Favorite.add(favorite.url, favorite.ip, :shingle => favorite.shingle, :color => favorite.color))}.to change{Favorite.count}.by(1)
-      expect{Favorite.add(favorite.url, favorite.ip, :shingle => favorite.shingle, :color => favorite.color))}.to change{Favorite.count}.by(0)
+      expect{Favorite.add(favorite.url, favorite.ip, :shingle => favorite.shingle, :color => favorite.color)}.to change{Favorite.count}.by(1)
+      expect{Favorite.add(favorite.url, favorite.ip, :shingle => favorite.shingle, :color => favorite.color)}.to change{Favorite.count}.by(0)
     end
   end
 end
